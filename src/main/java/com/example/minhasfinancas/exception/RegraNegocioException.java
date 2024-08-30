393f3718d95ee3475c0dd1a097fa647bd966e2f2
Paulo Moreira <nagatogts@hotmail.com>
2024-08-30 10:45:34 -0300

    Commit Inicial

---
+++ src/main/java/com/example/minhasfinancas/exception/RegraNegocioException.java
@@ -0,0 +1,7 @@
+package com.example.minhasfinancas.exception;
+
+public class RegraNegocioException extends RuntimeException {
+    public RegraNegocioException(String msg) {
+        super(msg);
+    }
+}
